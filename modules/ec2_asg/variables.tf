19bd4e1027d656a14b8bfca882d0108b6db0d313
Kasturtiranjan <kasturiranjan2711@gmail.com>
2026-09-06 19:15:22 +0530

    Create variables.tf

---
+++ modules/ec2_asg/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" { type = string }
+variable "private_app_subnet_ids" { type = list(string) }
+variable "app_sg_id" { type = string }
+variable "target_group_arn" { type = string }
+variable "instance_type" { type = string }
+variable "asg_min_size" { type = number }
+variable "asg_max_size" { type = number }
+variable "asg_desired_capacity" { type = number }
+variable "environment" { type = string }
